71936107d61d686d861c9cb57931d345df288bb5
Göran Häggsjö <goran.haggsjo@icloud.com>
2013-11-01 11:55:50 +0100

    finished and polished.

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ The first version is a dialog based game.
 
 After that we will add functionality to add graphics and eventhandlers for look&feel.
 
+The graphical game is in the 'index.html' it includes the CSS & js folders.
--- gui/js/main.js
+++
@@ -1,8 +0,0 @@
-/*jslint browser:true */
-/*global $: false, alert: false, confirm: false, console: false, Debug: false, opera: false, prompt: false, WSH: false */
-$(function () {
-    "use strict";
-    $(".row").click(function () {
-        $(this).hide();
-    });
-});
